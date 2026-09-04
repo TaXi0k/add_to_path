d7dcbad1093b51fda0b2b8fdecd20ba4c5db00b1
✩°｡⋆⸜ TaXi0k ✮ ᯓ★ <igorciu@interia.pl>
2026-09-05 00:24:46 +0200

    add functionality of sourcing from rc file after adding to path

---
+++ README.md
@@ -0,0 +1,3 @@
+![](/assets/header.png)
+
+**Add to path** is a simple and lightweight script enabling you to add things to `$PATH` with just one command. I created this app mostly due to the fact that I always forget how to add things to `$PATH` and end up googling (shame yep yep). Anyway, this very thing made me write this app in like half an hour and felt like sharing it here - maybe some of you have same problem, or just don't enjoy opening your `.rc` file everytime.
--- main.py
+++ main.py
@@ -13,25 +13,46 @@ def create_paths_file():
         open(file_path, "a").close() # create file
 
 
-def link_in_rc_file():
-    rc_paths = [
-        os.path.expanduser("~/.bashrc"),
-        os.path.expanduser("~/.zshrc")
-    ]
+def check_shell():
+    bash_path = os.path.expanduser("~/.bashrc")
+    zsh_path = os.path.expanduser("~/.zshrc")
+    
+    if os.path.isfile(bash_path):
+        return "bash"
+    elif os.path.isfile(zsh_path):
+        return "zsh"
+    else:
+        print(Style.BRIGHT + Fore.RED + f" Seems you are using shell other than ZSH or BASH. Only these two are supported :(")
+        exit()
+
+
+def link_in_rc_file(shell):
+    bash_path = os.path.expanduser("~/.bashrc")
+    zsh_path = os.path.expanduser("~/.zshrc")
 
     link_line = "\n# Created by add_to_path\nsource ~/.config/add_to_path/paths"
 
-    for rc_path in rc_paths:
-        if os.path.isfile(rc_path):
-            with open(rc_path, "r") as file:
-                content = file.read()
+    if shell == "bash":
+        with open(bash_path, "r") as file:
+            content = file.read()
+        
+        if link_line not in content:
+            with open(bash_path, "a") as file:
+                if content.endswith("\n"):
+                    file.write(f"{link_line}\n")
+                else:
+                    file.write(f"\n{link_line}\n")
 
-            if link_line not in content:
-                with open(rc_path, "a") as file:
-                    if content.endswith("\n"):
-                        file.write(f"{link_line}\n")
-                    else:
-                        file.write(f"\n{link_line}\n")
+    if shell == "zsh":
+        with open(zsh_path, "r") as file:
+            content = file.read()
+        
+        if link_line not in content:
+            with open(zsh_path, "a") as file:
+                if content.endswith("\n"):
+                    file.write(f"{link_line}\n")
+                else:
+                    file.write(f"\n{link_line}\n")
 
 
 def path_from_arg():
@@ -56,12 +77,21 @@ def append_to_paths_file(path):
                 print(Style.BRIGHT + Fore.GREEN + f" Successfully added {path} to $PATH!")
         else:
             print(Style.BRIGHT + Fore.GREEN + f" {path} already in $PATH!")
-    
+
+
+def source_rc_file(shell):
+    if shell == "bash":
+        os.system("source ~/.bashrc")
+    if shell == "zsh":
+        os.system("source ~/.zshrc")
+
 
 def main():
     create_paths_file()
-    link_in_rc_file()
+    shell = check_shell()
+    link_in_rc_file(shell)
     append_to_paths_file( path_from_arg() )
+    source_rc_file(shell)
 
 
 if __name__ == "__main__":
